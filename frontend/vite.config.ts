3ebfaa2ef63f7d3a07bda0d7d0b8cdcfada65d74
Muhammed Fais <pmuhammedfais2001@gmail.com>
2025-07-14 23:08:42 -1000

    Add vite as dev dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base:process.env.VITE_BASE_PATH || "/Fais-main-portfolio"
+  base:process.env.VITE_BASE_PATH || "/main-portfolio"
 }));
